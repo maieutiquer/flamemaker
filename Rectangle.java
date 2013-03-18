daf6d1e32d473c0f944a43ef6612ea1f15f5634a
Daniel Rodriguez <maieutiquer@gmail.com>
2013-03-18 06:44:27 +0100

    fix expandToAspectRatio

--- Rectangle.java
+++ Rectangle.java
@@ -6,28 +6,32 @@ public class Rectangle {
 	private final double height; 
 	
 	public Rectangle(Point center, double width, double height) throws IllegalArgumentException {
+		if (width<=0 || height<=0) {
+			throw new IllegalArgumentException("width or height less than or equal to 0");
+		}
 		this.center = center;
 		this.width = width;
 		this.height = height;
-		if (width<=0 || height<=0) {
-			throw new IllegalArgumentException();
-		}
 	}
 	
+	/**
+	 * 
+	 * @return the smallest coordinate x of the rectangle
+	 */
 	public double left() {
-		return center.x() - (this.width/2);
+		return this.center.x() - this.width/2;
 	}
 	
 	public double right() {
-		return center.x() + (this.width/2);
+		return this.center.x() + this.width/2;
 	}
 	
 	public double bottom() {
-		return center.y() - (this.height/2);
+		return this.center.y() - this.height/2;
 	}
 	
 	public double top() {
-		return center.y() + (this.height/2);
+		return this.center.y() + this.height/2;
 	}
 	
 	public double width() {
@@ -42,35 +46,55 @@ public class Rectangle {
 		return this.center;
 	}
 	
+	/**
+	 * Determines of a given point is inside the rectangle.
+	 * 
+	 * @param p
+	 * @return true if the point is inside the rectangle
+	 */
 	public boolean contains(Point p) {
 		return (p.x()>=this.left() && p.x()<this.right() 
 				&& p.y()>=this.bottom() && p.y()<this.top());
 	}
 	
+	/**
+	 * 
+	 * @return the width/height ratio of the rectangle
+	 */
 	public double aspectRatio() {
-		return width/height;
+		return this.width/this.height;
 	}
 	
 	public Rectangle expandToAspectRatio(double aspectRatio) throws IllegalArgumentException {
-		Point center = new Point(this.center().x(), this.center().y());
-		double width = 0;
-		double height = 0;
+		Point newCenter = new Point(this.center().x(), this.center().y());
+		double newWidth = 0;
+		double newHeight = 0;
 		
 		if (aspectRatio <= 0) {
-			throw new IllegalArgumentException();
-		}else if (aspectRatio > this.aspectRatio()) {
-			width = this.height*aspectRatio;
-			height = this.height;
-		}else if (aspectRatio < this.aspectRatio()) {
-			width = this.width;
-			height = this.width/aspectRatio;
+			throw new IllegalArgumentException("aspect ratio is negative or zero");
 		}
-		Rectangle rectangle = new Rectangle(center, width, height);
-		return rectangle;
+		/* not working code
+		else if (aspectRatio > this.aspectRatio()) {
+			newWidth = this.height*aspectRatio;
+			newHeight = this.height;
+		}else if (aspectRatio < this.aspectRatio()) {
+			newWidth = this.width;
+			newHeight = this.width/aspectRatio;
+			System.out.println(newHeight + " " + newWidth); //testing
+		}
+		*/
+		
+		newHeight = this.height;
+		newWidth = newHeight*aspectRatio;
+		newWidth = Math.max(this.width(), newWidth);
+		newHeight = newWidth/aspectRatio;
+		newHeight = Math.max(this.height(), newHeight);
+		
+		return new Rectangle(newCenter, newWidth, newHeight);
 	}
 	
 	public String toString() {
-		return "("+center.toString()+", "+this.width+", "+this.height+")";
+		return "("+this.center.toString()+", "+this.width+", "+this.height+")";
 	}
 	
 }
